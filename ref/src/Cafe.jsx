6aa577f74d349c43654d0d4b2e62cf995dcfe994
Yann Klein <yann.klein@me.com>
2024-02-27 23:01:16 +0900

    add badass shadow to the card

--- ref/src/Cafe.jsx
+++ ref/src/Cafe.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 
 function Cafe({cafe}) {
+  const gmapsUrl = `https://www.google.com/maps/search/?api=1&query=${cafe.title} ${cafe.address}`;
   return (
     <div className="col-12 col-md-6 col-xl-4 mb-4">
-      <div className="card h-100">
+      <div className="card h-100 shadow">
         <img height='260' src={cafe.picture} className="card-img-top object-fit-cover" alt={cafe.title} />
         <div className="card-body d-flex flex-column justify-content-between">
           <h5 className="card-title">{cafe.title}</h5>
           <p className="card-text">
             { cafe.criterion.slice(0, 4).map( crit => <span className="badge text-bg-secondary me-1" key={crit}>{crit}</span>)}
           </p>
-          <a href={cafe.gmaps_url} className="btn btn-success">Show the map 📍</a>
+          <a target="_blank" href={gmapsUrl} className="btn btn-success">Show the map 📍</a>
         </div>
       </div>
     </div>
