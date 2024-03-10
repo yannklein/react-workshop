e045f5bd22d460a08db023870e51c8abd1d8fce7
Yann Klein <yann.klein@me.com>
2024-03-10 13:40:40 +0900

    change vite config to match repo renaming

--- ref/vite.config.js
+++ ref/vite.config.js
@@ -1,8 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/* eslint-disable no-undef */
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/react-workshop-ref-feb2024/"
-})
+  base: "/react-workshop-ref/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nested: resolve(__dirname, 'slides.html'),
+      },
+    },
+  },
+});
