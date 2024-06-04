73d5b4409f9b0df284a0c872850d48921e16db50
Yann Klein <yann.klein@me.com>
2024-06-04 13:27:22 +0900

    change use effect to normal event + fecth

--- ref/src/App.jsx
+++ ref/src/App.jsx
@@ -5,7 +5,30 @@ import Sidebar from './components/Sidebar/Sidebar';
 import CafeList from './components/CafeList/CafeList';
 
 function App() {
-  const [cafes, setCafes] = React.useState([]);
+  const [cafes, setCafes] = React.useState([
+    {
+      picture:
+        'https://laptopfriendly.co/images/places/tokyo/ddsk-saigon-kitchen/ddsk-saigon-kitchen--tokyo.jpg',
+      title: 'DDSK SAIGON KITCHEN',
+      address: 'ss, 135-0061, Tokyo',
+      criteria: [],
+    },
+    {
+      picture:
+        'https://laptopfriendly.co/images/places/tokyo/common/common--tokyo.jpg',
+      title: 'COMMON',
+      address: '106-0032, Tokyo',
+      criteria: ['Stable Wi-Fi', 'Power sockets', 'Quiet', 'Coffee', 'Food'],
+    },
+    {
+      picture:
+        'https://laptopfriendly.co/images/places/tokyo/brooklyn-parlor-shinjuku/brooklyn-parlor-shinjuku%20shinjuku-city%20brooklyn-parlor-shinjuku-shinjuku-city%20.jpg',
+      title: 'BROOKLYN PARLOR SHINJUKU',
+      address:
+        'Shinjuku Marui Annex B1F, 3-1-26 Shinjuku, Shinjuku-ku, Tokyo, 160-0022, Tokyo',
+      criteria: ['Stable Wi-Fi', 'Power sockets', 'Quiet', 'Coffee', 'Food'],
+    },
+  ]);
   // OPTIONAL: USE EFFECT VERSION
   // const [keyword, setKeyword] = React.useState("");
 
@@ -20,13 +43,13 @@ function App() {
   // }, [keyword]);
 
   return (
-    <div className='app-frame'>
+    <div className="app-frame">
       {/* OPTIONAL USE EFFECT VERSION  */}
       {/* <Navbar setKeyword={setKeyword}/> */}
-      <Navbar setCafes={setCafes}/>
+      <Navbar setCafes={setCafes} />
       <div className="app-body">
         <Sidebar setCafes={setCafes} />
-        <CafeList cafes={cafes}/>
+        <CafeList cafes={cafes} />
       </div>
     </div>
   );
