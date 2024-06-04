63cd390214559d485076f1e74fcd3268b3516476
Yann Klein <yann.klein@me.com>
2024-06-04 13:22:07 +0900

    remove main.jsx code

--- workshop/src/main.jsx
+++ workshop/src/main.jsx
@@ -1,8 +1,1 @@
 import './index.css';
-
-import * as ReactDOM from 'react-dom/client';
-import App from './App';
-
-const rootDiv = document.querySelector("#root");
-const root = ReactDOM.createRoot(rootDiv);
-root.render(<App />);
