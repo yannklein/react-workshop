700c0c8e56b3f3386516c45856551c48b252f07f
Yann Klein <yann.klein@me.com>
2024-06-04 12:39:18 +0900

    add css to app

--- workshop/src/main.jsx
+++ workshop/src/main.jsx
@@ -1,1 +1,8 @@
 import './index.css';
+
+import * as ReactDOM from 'react-dom/client';
+import App from './App';
+
+const rootDiv = document.querySelector("#root");
+const root = ReactDOM.createRoot(rootDiv);
+root.render(<App />);
